6628eb7841c282c5ea5a73d8048a2b0b88d67dfa
jsinger03 <julian.singer@gmail.com>
2020-11-01 19:21:21 -0500

    completed human mode reader

--- XORString.py
+++ XORString.py
@@ -17,38 +17,32 @@ def xorstring(mode, input_file, key_file):
 def humanxor(input, key):
     output = ""
     toReturn=""
-    # input = input.encode()
-    # print("hopefully this works : ")
-    # #print(int("0x", 16))
-    # print(eval(input))
-    print("length of input is : " + str(len(input)))
-    print("length of key is : " + str(len(key)))
+    # print("length of input is : " + str(len(input)))
+    # print("length of key is : " + str(len(key)))
     key_length_adjusted = ""
     while len(input) - len(key_length_adjusted) > 0:
         if len(input) - len(key) > len(key):
             key_length_adjusted = key_length_adjusted + key
         elif len(input) - len(key) < len(key):
             length_to_add = len(input) % len(key)
-            print(length_to_add)
+            # print(length_to_add)
             key_length_adjusted = key_length_adjusted + key[0:length_to_add]
-    input_hexadecimal = ""
     for i in range(len(input)):
         print("input[i] is: " + input[i] + ", key[i] is: " + key_length_adjusted[i])
         print(hex(ord(input[i])))
         print(hex(ord(key_length_adjusted[i])))
         print(hex(int(hex(ord(input[i])), 16) ^ int(hex(ord(key_length_adjusted[i])), 16)))
-        output = output + str(hex(int(hex(ord(input[i])), 16) ^ int(hex(ord(key_length_adjusted[i])), 16)))
-    #print("input as hexadecimal is : " + str(type(input_hexadecimal)));
-    key_hexadecimal = "";
+        toAdd = str(hex(int(hex(ord(input[i])), 16) ^ int(hex(ord(key_length_adjusted[i])), 16)))
+        print("the XOR result is : " + str(bytearray.fromhex(toAdd[2:4]).decode()))
+        output = output + toAdd
 
     print(output)
-    # for l in range(0, len(output), 2):
-    #     print(output[l:l+2])
-    #     print(bytearray.fromhex(output[l:l+2]).decode())
-    #     toReturn = toReturn + str(bytearray.fromhex(output[l:l+2]).decode())
-    # print(toReturn)
-    print(bytearray.fromhex(output).decode())
+    for l in range(0, len(output), 4):
+        # print(output[l:l+4])
+        # print(bytearray.fromhex(output[l+2:l+4]).decode())
+        toReturn = toReturn + str(bytearray.fromhex(output[l+2:l+4]).decode())
+    print("this is toReturn: " + toReturn)
+    print("desired output: " + " this is a test")
+    return toReturn
 
-
-print(ord("h") ^ ord("A"))
 xorstring("human", "input.txt", "key.txt")
